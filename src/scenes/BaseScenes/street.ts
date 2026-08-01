27c7280625465497c316d419202b2ced0151d347
Kimnkitae <kimnkitae@gmail.com>
2026-08-01 16:17:43 +0300

    fix: fail(couldn't add colliders properly)

--- src/scenes/BaseScenes/street.ts
+++ src/scenes/BaseScenes/street.ts
@@ -1,4 +1,6 @@
 import Phaser from 'phaser'
+import Player from '../../utils/player/player'
+
 
 export default class Chapter1BaseStreetScene extends Phaser.Scene {
     spacebar!: Phaser.Input.Keyboard.Key
@@ -6,6 +8,7 @@ export default class Chapter1BaseStreetScene extends Phaser.Scene {
     walls!: Phaser.Physics.Arcade.StaticGroup
     bench!: Phaser.Physics.Arcade.StaticGroup
     trees!: Phaser.Physics.Arcade.StaticGroup
+    
 
 
     constructor(config: Phaser.Types.Scenes.SettingsConfig) {
@@ -14,6 +17,9 @@ export default class Chapter1BaseStreetScene extends Phaser.Scene {
 
     create() {
         this.spacebar = this.input.keyboard!.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
+        
+        
+        
         this.add.image(0, 0, 'chapter1street').setOrigin(-0.2, -0.2)
 
         this.house = this.physics.add.staticGroup()
@@ -21,13 +27,17 @@ export default class Chapter1BaseStreetScene extends Phaser.Scene {
         this.bench = this.physics.add.staticGroup()
         this.trees = this.physics.add.staticGroup()
 
-        this.walls.create(0, 0, 'chapter1streetLeftWall')
-        this.walls.create(0, 0, 'chapter1streetTopWall')
-        this.walls.create(0, 0, 'chapter1streetRightWall')
-        this.walls.create(0, 0, 'chapter1streetBottomWall')
+    
+        this.walls.create(179, 0, 'chapter1streetLeftWall').setOrigin(-0.2, -0.2)
+        this.walls.create(92, 65, 'chapter1streetTopWall').setOrigin(-0.2, -0.2)
+        this.walls.create(615, 0, 'chapter1streetRightWall').setOrigin(-0.2, -0.2)
+        this.walls.create(92, 418, 'chapter1streetBottomWall').setOrigin(-0.2, -0.2)
+        this.trees.create(160, 52, 'chapter1streetTrees').setOrigin(-0.2, -0.2).setData('textKey', 'trees')
+        this.house.create(120, 372, 'chapter1streetHouse').setOrigin(-0.2, -0.2).setData('textKey', 'house')
+        this.bench.create(210, 216, 'chapter1streetBench').setOrigin(-0.2, -0.2).setData('textKey', 'bench')
 
-        this.trees.create(0, 0, 'chapter1streetTrees')
-        this.house.create(0, 0, 'chapter1streetHouse')
-        this.bench.create(0, 0, 'chapter1streetBench')
+
+        
+        
     }
 }
